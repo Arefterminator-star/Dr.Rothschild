9ea51b68d73f6ce03e79f026fa7fbec48583bfd2
Arefterminator-star <76777630+Arefterminator-star@users.noreply.github.com>
2020-12-30 16:49:27 +0330

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("NzkzODAzMDQ3NjY2OTc0NzUw.X-xksg.KBNB84jnpVnfB5uUNNIALW0KJsY")                
+client.run("NzkzODAzMDQ3NjY2OTc0NzUw.X-xksg.j7JE00N8lFmo6Myri6nQAMHFjZQ")                
